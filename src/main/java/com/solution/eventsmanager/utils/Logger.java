4db98300491ef0cee90876b0a7e07c3dadff3043
Dmitriy Shishkov <shishkov@netcracker.com>
2020-05-10 22:23:43 +0400

    add wa for logging

--- src/main/java/com/solution/eventsmanager/utils/Logger.java
+++ src/main/java/com/solution/eventsmanager/utils/Logger.java
@@ -1,4 +1,18 @@
 package com.solution.eventsmanager.utils;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Logger {
+    public void info(String str){
+        try(FileWriter writer = new FileWriter("app.log", true))
+        {
+            writer.write(str);
+            writer.write("\n\t");
+            writer.flush();
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
 }
