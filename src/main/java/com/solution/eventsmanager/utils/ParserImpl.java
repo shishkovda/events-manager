fc5566cfc5f4e29e8c5b2d74bb29b9ccb6e0fc60
Dmitriy Shishkov <shishkov@netcracker.com>
2020-05-15 09:31:12 +0400

    add logs

--- src/main/java/com/solution/eventsmanager/utils/ParserImpl.java
+++ src/main/java/com/solution/eventsmanager/utils/ParserImpl.java
@@ -14,17 +14,23 @@ public class ParserImpl {
     public List<String> parse(String message) {
         logger.info("parse(): message = " + message);
         message = message.replaceAll("\"", "");
+        logger.info("parse(): message = " + message);
 
         int idx1, idx2;
 
         idx1 = message.indexOf(" ", message.indexOf("Зачисление"));
         idx2 = message.indexOf(" ", idx1+1);
+        logger.info("parse(): idx1 = " + idx1);
+        logger.info("parse(): idx2 = " + idx2);
 
         List<String> attributeValues = new ArrayList<>();
         attributeValues.add(message.substring(idx1+1, idx2));
+        logger.info("parse(): attributeValues = " + attributeValues);
 
         idx1 = message.indexOf(" ", message.indexOf("отправителя"));
         attributeValues.add(message.substring(idx1+1));
+        logger.info("parse(): idx1 = " + idx1);
+        logger.info("parse(): attributeValues = " + attributeValues);
 
         return attributeValues;
     }
